b5eeef5542f6558845ad047dc84e0eb74c5957fe
Maylis <maylis.ameto@gmail.com>
2023-06-02 11:47:35 +0200

    Video speed

--- index.php
+++ index.php
@@ -13,6 +13,13 @@
     <video id="bg_video" autoplay loop muted>
         <source src="./assets/clouds.mp4" type="video/mp4">
     </video>
+    <script>
+        let video = document.getElementById('bg_video')
+        function decrease (video) {
+            video.playbackRate = 0.75;
+        }
+        decrease(video);
+    </script>
 <header>
 </header>
 <?php
