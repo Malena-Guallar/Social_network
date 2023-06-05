54e5c82891bca67bdc2bf0f0ee0e2ff4abc283b3
Maylis <maylis.ameto@gmail.com>
2023-06-05 16:55:45 +0200

    (Fix) Fix style

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<style><?php
-    include("index.css")
-?></style>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Afterlife</title>
+    <link rel="stylesheet" href="index.css?<?php echo time(); ?>"/>
 </head>
 <?php include('video.php') ?>
 <body>
