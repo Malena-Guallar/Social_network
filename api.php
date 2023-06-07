1fd6a66ca9b981b2028cc5d4b900661816a0de0e
Malena Guallar <malena.guallar@gmail.com>
2023-06-07 14:40:18 +0200

    doc appel API créé

---
+++ api.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Afterlife</title>
+    <link rel="stylesheet" href="index.css?<?php echo time(); ?>"/>
+</head>
+<?php include('video.php') ?>
+<body>
+<header>
+<?php
+    include_once("header.php");
+?>
+</header>
+
+
+
+<p> coucou </p>
+
+
+
+
+<footer>
+
+</footer>
+</body>
+</html>
